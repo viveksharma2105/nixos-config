542ce290712e905e540db3158b8d6b9c4d1a82ad
installer <installer@gmail.com>
2024-10-21 01:19:26 -0500

    Init

---
+++ hosts/viri/variables.nix
@@ -0,0 +1,16 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Vivek Sharma";
+  gitEmail = "viveksharma02005@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
