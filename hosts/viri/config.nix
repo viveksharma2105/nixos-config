04dc2fa1e04d443a04d3d7a2187fe5b9f87e9a7e
vasujain275 <vasujain275@gmail.com>
2024-10-21 01:32:15 -0500

    Added Grub

--- hosts/viri/config.nix
+++ hosts/viri/config.nix
@@ -22,21 +22,27 @@ in
   ];
 
   boot = {
-    # Kernel
     kernelPackages = pkgs.linuxPackages_zen;
-    # This is for OBS Virtual Cam Support
     kernelModules = [ "v4l2loopback" ];
     extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    # Needed For Some Steam Games
     kernel.sysctl = {
       "vm.max_map_count" = 2147483642;
     };
-    # Bootloader.
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = true;
+      };
+    };
     # Make /tmp a tmpfs
     tmp = {
-      useTmpfs = false;
+      useTmpfs = true;
       tmpfsSize = "30%";
     };
     # Appimage Support
